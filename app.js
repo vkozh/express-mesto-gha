2b853f1665ab9ee2d94639ad949cabbacc225603
Lekozhe <v.a.kozh@ya.ru>
2022-06-18 19:43:03 +0300

    feat: create server and bd connection

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const router = require("./routes/routes");
 const bodyParser = require("body-parser");
 const user = require("./models/user");
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect("mongodb://localhost:27017/mestodb", {
-  // useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-});
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
 const app = express();
 
@@ -18,11 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", (req, res, next) => {
   req.user = {
-    _id: "62aa3a2dfaf32a08b0ae2749",
+    _id: "62ac9d0ed557e84d70fc73d6",
   };
   next();
 });
-// app.use("/", router);
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
 
