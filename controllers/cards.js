bd35aa505068e1d5d37e1824167754de587e204f
Lekozhe <v.a.kozh@ya.ru>
2022-06-18 19:40:07 +0300

    feat: controllers for users and cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,16 +1,18 @@
+const { default: mongoose } = require("mongoose");
 const Card = require("../models/card");
+const { ERRORS, MESSAGES } = require("../utils/constants");
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send(card))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+    .then((card) => res.send(getCardData(card)))
+    .catch((err) => catchErr(err, res, MESSAGES.errorCardCreate));
 };
 
 module.exports.deleteCard = (req, res) =>
-  Card.findByIdAndRemove(req.params.id)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send(getCardData(card)))
+    .catch((err) => catchErr(err, res, MESSAGES.cardNotFound));
 
 module.exports.setLike = (req, res) =>
   Card.findByIdAndUpdate(
@@ -18,8 +20,8 @@ module.exports.setLike = (req, res) =>
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+    .then((card) => res.send(getCardData(card)))
+    .catch((err) => catchErr(err, res, MESSAGES.errorSetLike));
 
 module.exports.deleteLike = (req, res) =>
   Card.findByIdAndUpdate(
@@ -27,11 +29,69 @@ module.exports.deleteLike = (req, res) =>
     { $pull: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+    .then((card) => res.send(getCardData(card)))
+    .catch((err) => catchErr(err, res, MESSAGES.errorRemoveLike));
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+    .then((cards) => res.send(cards.map(getCardData)))
+    .catch((err) => catchErr(err, res));
 };
+
+module.exports.getCard = (req, res) => {
+  Card.findById(req.params.cardId)
+    .then((card) => res.send(getCardData(card)))
+    .catch((err) => catchErr(err, res, MESSAGES.cardNotFound));
+};
+
+const validation = (err, message = err.message) => {
+  switch (err.name) {
+    case "ValidationError":
+      return new CardValidationError(message);
+    case "CastError":
+      return new CardCastError(message);
+    default:
+      return new ServerError(err.message);
+  }
+};
+
+const catchErr = (err, res, msg) => {
+  const { statusCode, message } = validation(err, msg);
+  res.status(statusCode).send({ message });
+};
+
+const getCardData = (card) => {
+  if (card)
+    return {
+      name: card.name,
+      link: card.link,
+      likes: card.likes,
+      _id: card._id,
+      owner: card.owner,
+    };
+  throw new CardCastError(MESSAGES.cardNotFound);
+};
+
+class CardCastError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "CardCastError";
+    this.statusCode = ERRORS.NOT_FOUND;
+  }
+}
+
+class CardValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "CardValidationError";
+    this.statusCode = ERRORS.UNCORRECT;
+  }
+}
+
+class ServerError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ServerError";
+    this.statusCode = ERRORS.SERVER_ERR;
+  }
+}
--- controllers/users.js
+++ controllers/users.js
@@ -1,51 +1,93 @@
 const User = require("../models/user");
+const { ERRORS, MESSAGES } = require("../utils/constants");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
-  // next();
+    .then((users) => res.send(users.map(getUserData)))
+    .catch((err) => catchErr(res, err, MESSAGES.userNotFound));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
-  // next();
+    .then((user) => res.send(getUserData(user)))
+    .catch((err) => catchErr(res, err, MESSAGES.userNotFound));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
-  // next();
+    .then((user) => res.send(getUserData(user)))
+    .catch((err) => catchErr(res, err, MESSAGES.errorUserCreate));
 };
-
-// module.exports.doesUserExist = (req, res, next) => {
-//   const { userId } = req.params.userId;
-//   if (!userId) {
-//     res.send("Такого пользователя не существует.");
-//     return;
-//   }
-//   next();
-// };
-
-// module.exports.sendUsers = (req, res, next) => {
-//   const { users } = req.body;
-//   res.send(users);
-//   next();
-// };
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-    .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
+    .then((user) => res.send(getUserData(user)))
+    .catch((err) => catchErr(res, err, MESSAGES.errorProfileUpdate));
 };
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .then((newAvatar) => res.send(newAvatar))
-    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true }
+  )
+    .then((user) => res.send(getUserData(user)))
+    .catch((err) => catchErr(res, err, MESSAGES.errorAvatarUpdate));
 };
+
+const validation = (err, message = err.message) => {
+  switch (err.name) {
+    case "ValidationError":
+      return new UserValidationError(message);
+    case "CastError":
+      return new UserCastError(message);
+    default:
+      return new ServerError(err.message);
+  }
+};
+
+const catchErr = (res, err, msg) => {
+  const { statusCode, message } = validation(err, msg);
+  return res.status(statusCode).send({ message });
+};
+
+const getUserData = (user) => {
+  if (user)
+    return {
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      _id: user._id,
+    };
+  throw new UserCastError(MESSAGES.userNotFound);
+};
+
+class UserCastError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UserCastError";
+    this.statusCode = ERRORS.NOT_FOUND;
+  }
+}
+
+class UserValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UserValidationError";
+    this.statusCode = ERRORS.UNCORRECT;
+  }
+}
+
+class ServerError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ServerError";
+    this.statusCode = ERRORS.SERVER_ERR;
+  }
+}
