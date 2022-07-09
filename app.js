5d846ea0dbb5670d7494f1dfc8b27650ef46f3e6
Lekozhe <v.a.kozh@ya.ru>
2022-07-09 20:35:59 +0300

    tests not found changes

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
+const {
+  celebrate, Joi, errors,
+} = require('celebrate');
+const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { MESSAGES } = require('./utils/constants');
 
@@ -16,6 +19,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use('/', auth, (req, res, next) => next());
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?\S+\.\w+(\/\S+)*#?/),
+
+  }),
+}), login);
+app.post('/signup', celebrate(
+  {
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?\S+\.\w+(\/\S+)*#?/),
+    }),
+  },
+), createUser);
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
 
@@ -23,6 +47,12 @@ app.use(errors());
 app.use((req, res) => res.status(404).send({ message: MESSAGES.wrongPath }));
 app.use((err, req, res, next) => {
   const { statusCode, message } = err;
+  // if (isCelebrateError(err)) {
+  //   const errorBody = err.details.get('body');
+  //   const { details: [errorDetails] } = errorBody;
+  //   statusCode = ERRORS.UNAUTHORIZED;
+  //   message = errorDetails.message;
+  // }
   res.status(statusCode).send({ message });
   next();
 });
--- routes/users.js
+++ routes/users.js
@@ -6,29 +6,7 @@ const {
   updateProfile,
   updateAvatar,
   getProfile,
-  login,
-  createUser,
 } = require('../controllers/users');
-
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required(),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?\S+\.\w+(\/\S+)*#?/),
-  }),
-}), login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?\S+\.\w+(\/\S+)*#?/),
-  }),
-}), createUser);
 
 router.get('/', getUsers);
 router.get('/me', getProfile);
